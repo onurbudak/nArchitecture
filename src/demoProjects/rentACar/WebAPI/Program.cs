c8a1f2a60a0514aa2ceb4f3b28e846fa5eccde50
Onur Budak <onurbudak1997@outlook.com>
2023-02-12 23:13:29 +0300

    CQRS yapısı eklendi.

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
